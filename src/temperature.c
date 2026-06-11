0352ed5da4e2a296f85735c9eb63ed249b2c9e51
Ronan <ronan.le_meillat@highcanfly.club>
2026-06-11 19:09:27 +0200

    feat: add MIT license headers to project files
    Add MIT license headers to Makefile, cache_stress.c, cache_stress.h, and cpu_stress.c files to ensure proper copyright and licensing information is included in all source files. This change provides clear legal terms for software usage, modification, and distribution while protecting the authors and copyright holders from liability.

--- src/temperature.c
+++ src/temperature.c
@@ -22,6 +22,26 @@
  *   lut[0x85=133]=259 -> 25.9 C  (AIDA64 Aux=25 C)
  *   lut[0xD9=217]=759 -> 75.9 C  (AIDA64 CPU=73 C)
  *   lut[0xC6=198]=592 -> 59.2 C  (AIDA64 MB =57 C)
+ *
+ * Copyright (c) 2026 Ronan LE MEILLAT - SCTG Development for ISMO Group Ltd.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *****************************************************************************/
 
 #include "temperature.h"
